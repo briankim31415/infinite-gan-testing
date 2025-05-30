c2275ffdc4e0b76b187cf25be395524ab56c74b0
brian <briankim31415@gmail.com>
2025-05-30 15:06:09 -0500

    Create run_batch.bat

---
+++ run_batch.bat
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -J sde_gan_test
+#SBATCH -o run_logs/job.o%j
+#SBATCH -e run_logs/job.e%j
+#SBATCH -p gh
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 05:00:00
+#SBATCH --mail-type=all
+#SBATCH --mail-user=briankim31415@gmail.com
+#SBATCH -A CCR25007
+
+module load gcc cuda python3
+source ./inf_gan/bin/activate
+python3 examples/sde_gan.py
